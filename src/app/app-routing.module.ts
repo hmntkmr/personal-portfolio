c75fddef6e7adbbd4c8b6de25b8fb3d4f60e09ef
hmntkmr <hemantkumar@me.com>
2018-04-23 04:50:16 -0400

    routing module working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { BlogComponent } from './blog/blog.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent},
+  { path: 'profile', component: ProfilePageComponent},
+  { path: 'blog', component: BlogComponent},
+  { path: 'contact', component: ContactComponent}
+  // { path: '**', component: PageNotFountComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      {enableTracing: true} 
+    )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
